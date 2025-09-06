9db3ad6a8b17dad9ba9c0728210163b4555a3ff6
Dobbymin <gmin990624@gmail.com>
2025-09-07 00:52:05 +0900

    feat: not found 페이지 추가

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h1 className='text-5xl font-bold'>페이지를 찾을 수 없습니다.</h1>
+      <span>잘못된 경로로 접근하셨습니다. 주소를 다시 확인해 주세요.</span>
+    </div>
+  );
+}
